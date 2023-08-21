bebadd8a79c59e84e735ba22750458b08c61b221
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-21 14:58:31 -0300

    Anatomia das Classes 2: Testando nome de variáveis e criação de constante.

---
+++ java-anatomia-classes/src/MinhaClasseAula2.java
@@ -0,0 +1,13 @@
+public class MinhaClasseAula2 {
+    public static void main(String[] args) {
+
+        final String BR = "Brasil";
+        BR = "Brazil";
+
+        int ano = 2022;
+        ano = 2023;
+
+        double $VALOR = 3.99;
+
+    }
+}
