649967e5671c89cef18f82c9c203073849f08382
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-21 15:50:15 -0300

    Tipos e variáveis 4: Trabalhando com tipagem.

---
+++ java-anatomia-classes/src/edu/patrick/sintaxeJava/tiposEVariaveis/TiposVariaveis.java
@@ -0,0 +1,13 @@
+package edu.patrick.sintaxeJava.tiposEVariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2_000;
+
+        System.out.println(salarioMinimo);
+
+        short numero1 = 1;
+        int numeroInteiro = numero1;
+        // short numero2 = numeroInteiro;
+    }
+}
