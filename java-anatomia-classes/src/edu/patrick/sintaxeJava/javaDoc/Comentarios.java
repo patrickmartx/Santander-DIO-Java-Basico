0567e9e4e6b4a5678822269fd5ebffb3e67f5f37
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-24 12:17:26 -0300

    Java doc 2: Criado uma classe mal estruturada para comparar a documentação.

---
+++ java-anatomia-classes/src/edu/patrick/sintaxeJava/javaDoc/Comentarios.java
@@ -0,0 +1,9 @@
+package edu.patrick.sintaxeJava.javaDoc;
+
+public class Comentarios {
+    // esse aqui é um comentário
+    public int somar(int n, int m){
+        int r = n + m;
+        return r;
+    }
+}
