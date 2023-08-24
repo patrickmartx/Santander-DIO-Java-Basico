7ba1350aa197ecc267eb48619c8eaceb9275f64c
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-24 10:29:42 -0300

    Operadores 5: Usando ternário.

---
+++ java-anatomia-classes/src/edu/patrick/sintaxeJava/operadores/OperadoresAula5.java
@@ -0,0 +1,12 @@
+package edu.patrick.sintaxeJava.operadores;
+
+public class OperadoresAula5 {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        System.out.println(a == b ? "São iguais" : "São diferentes");
+    }
+}
