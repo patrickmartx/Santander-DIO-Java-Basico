eeea3658f9a68179709028bd63a3f102c47b0f47
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-24 10:16:05 -0300

    Operadores 3: Dicas para usar os operadores para encremento e tornar negativo.

---
+++ java-anatomia-classes/src/edu/patrick/sintaxeJava/operadores/OperadoresAula2.java
@@ -0,0 +1,10 @@
+package edu.patrick.sintaxeJava.operadores;
+
+public class OperadoresAula2 {
+    public static void main(String[] args) {
+        int numero = 5;
+        numero++;
+
+        System.out.println(- numero);
+    }
+}
