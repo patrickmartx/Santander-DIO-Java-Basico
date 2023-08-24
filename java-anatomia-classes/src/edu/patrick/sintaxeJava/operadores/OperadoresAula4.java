c6d3aa49642a2003cb3ce9c3d07e90dc0c6015f2
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-24 10:23:54 -0300

    Operadores 4: Fazendo negação através de !

---
+++ java-anatomia-classes/src/edu/patrick/sintaxeJava/operadores/OperadoresAula4.java
@@ -0,0 +1,9 @@
+package edu.patrick.sintaxeJava.operadores;
+
+public class OperadoresAula4 {
+    public static void main(String[] args) {
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+    }
+}
