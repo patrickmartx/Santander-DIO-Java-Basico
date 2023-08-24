f1fb0154e018096be197c3c473802b519955008e
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-24 12:17:52 -0300

    doc: Criado para utilização do java doc

---
+++ java-anatomia-classes/src/edu/patrick/sintaxeJava/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.patrick.sintaxeJava.javaDoc","l":"Calculadora"},{"p":"edu.patrick.sintaxeJava.javaDoc","l":"Comentarios"}];updateSearchResults();
