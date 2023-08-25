035229f0bc64d609907ba8376a0e30706e982eb1
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-25 14:38:17 -0300

    Condicional Composta: Utilização de if e else na aplicação.

---
+++ java-anatomia-classes/src/edu/patrick/sintaxeJava/estruturasCondicionais/CondicionaisCompostas.java
@@ -0,0 +1,12 @@
+package edu.patrick.sintaxeJava.estruturasCondicionais;
+
+public class CondicionaisCompostas {
+    public static void main(String[] args) {
+        int media = 6;
+
+        if (media > 7) 
+            System.out.println("Aprovado!");
+        else
+            System.out.println("Reprovado!");
+    }
+}
