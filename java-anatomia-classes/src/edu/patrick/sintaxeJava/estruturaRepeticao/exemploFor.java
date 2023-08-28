61e31b6ce23ade5325f217688a940f804198c671
patrick-martins <xavier.patrickmartins@gmail.com>
2023-08-28 12:29:32 -0300

    For 1: Utilizando for para repetição

---
+++ java-anatomia-classes/src/edu/patrick/sintaxeJava/estruturaRepeticao/exemploFor.java
@@ -0,0 +1,11 @@
+package edu.patrick.sintaxeJava.estruturaRepeticao;
+
+public class exemploFor {
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
+            System.out.printf("Quantidade de carneirinhos: %d\n",carneirinhos);
+        }
+
+        System.out.println("Joãozinho dormiu!");
+    }
+}
