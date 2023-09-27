97f7af2c42d2adf8780f39ab7c95b902f37b1040
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-09-27 19:06:10 -0300

    Criada classe FunctionExample.java

--- StreamAPI/src/edu/patrick/StreamAPI/FunctionExample.java
+++ StreamAPI/src/edu/patrick/StreamAPI/FunctionExample.java
@@ -14,11 +14,11 @@ public class FunctionExample {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
         // Usar a Function com expressão lambda para dobrar todos os números
-        Function<Integer, Integer> dobrar = numero -> numero * 2;
+        // Function<Integer, Integer> dobrar = numero -> numero * 2;
 
         // Usar a função para dobrar todos os números no Stream e armazená-los em outra lista
         List<Integer> numerosDobrados = numeros.stream()
-                .map(dobrar)
+                .map(n -> n * 2)
                 .toList();
 
         // Imprimir a lista de números dobrados
