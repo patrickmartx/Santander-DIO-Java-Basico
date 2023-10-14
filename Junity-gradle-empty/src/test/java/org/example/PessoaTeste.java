e79e4cca885a18a423438b9565db2f22eb1e4c7d
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-10-14 12:48:45 -0300

    Teste Junity com Gradle

--- Junity-gradle-empty/src/test/java/org/example/PessoaTeste.java
+++ Junity-gradle-empty/src/test/java/org/example/PessoaTeste.java
@@ -7,17 +7,37 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
 public class PessoaTeste {
+
+    Pessoa pessoa;
     int idadeEsperada;
+    String nome;
+    LocalDate nascimento;
 
     @BeforeEach
     public void preencheVars(){
+        nome = "Julia";
+        nascimento = LocalDate.of(2020, 1, 1);
+        
+        pessoa = new Pessoa(nome, nascimento);
         idadeEsperada = 3;
     }
 
     @Test
-    void validarCalculoDeIdade() {
-        Pessoa pessoa = new Pessoa("Julia", LocalDate.of(2020, 1, 1));
+    void getNomeTeste() {
+        String nomeEsperado = pessoa.getNome();
 
+        Assertions.assertEquals(nome, nomeEsperado);
+    }
+    
+    @Test
+    void getNascimentoTeste() {
+        LocalDate nascimentoEsperado = pessoa.getNascimento();
+        
+        Assertions.assertEquals(nascimento, nascimentoEsperado);
+    }
+
+    @Test
+    void validarCalculoDeIdade() {
         Assertions.assertEquals(idadeEsperada, pessoa.getIdade());
     }
 }
